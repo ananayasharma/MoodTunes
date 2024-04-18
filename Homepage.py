36149914e9adfb39d500af5e56dd05be3c0731db
ananayasharma <101507735+ananayasharma@users.noreply.github.com>
2024-04-19 00:01:34 +0530

    Add files via upload

---
+++ Homepage.py
@@ -0,0 +1,261 @@
+from pathlib import Path
+#Imports for streamlit
+import streamlit as st
+import av
+import cv2
+from streamlit_webrtc import (
+    RTCConfiguration,
+    VideoProcessorBase,
+    WebRtcMode,
+    webrtc_streamer,
+)
+from streamlit_extras.switch_page_button import switch_page
+from streamlit_extras.app_logo import add_logo
+
+#Imports for ml model
+import numpy as np
+import mediapipe as mp
+from keras.models import load_model
+
+
+st.set_page_config(
+    page_title="MoodTunes",
+    page_icon="🎵",
+)
+
+page_bg_img = """
+<style>
+
+div.stButton > button:first-child {
+    all: unset;
+    width: 120px;
+    height: 40px;
+    font-size: 32px;
+    background: transparent;
+    border: none;
+    position: relative;
+    color: #f0f0f0;
+    cursor: pointer;
+    z-index: 1;
+    padding: 10px 20px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    white-space: nowrap;
+    user-select: none;
+    -webkit-user-select: none;
+    touch-action: manipulation;
+
+}
+div.stButton > button:before, div.stButton > button:after {
+    content: '';
+    position: absolute;
+    bottom: 0;
+    right: 0;
+    z-index: -99999;
+    transition: all .4s;
+}
+
+div.stButton > button:before {
+    transform: translate(0%, 0%);
+    width: 100%;
+    height: 100%;
+    background: #0f001a;
+    border-radius: 10px;
+}
+div.stButton > button:after {
+  transform: translate(10px, 10px);
+  width: 35px;
+  height: 35px;
+  background: #ffffff15;
+  backdrop-filter: blur(5px);
+  -webkit-backdrop-filter: blur(5px);
+  border-radius: 50px;
+}
+
+div.stButton > button:hover::before {
+    transform: translate(5%, 20%);
+    width: 110%;
+    height: 110%;
+}
+
+
+div.stButton > button:hover::after {
+    border-radius: 10px;
+    transform: translate(0, 0);
+    width: 100%;
+    height: 100%;
+}
+
+div.stButton > button:active::after {
+    transition: 0s;
+    transform: translate(0, 5%);
+}
+
+
+
+
+
+[data-testid="stAppViewContainer"] {
+background-image: url("https://images.unsplash.com/photo-1615840636404-0f2412fd2732?q=80&w=1906&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D");
+background-size: cover;
+background-position: top left;
+background-repeat: no-repeat;
+background-attachment: local;
+}
+
+[data-testid="stSidebar"] > div:first-child {
+background-position: center; 
+background-repeat: no-repeat;
+background-attachment: fixed;
+background :#c1d9f2;
+}
+
+[data-testid="stHeader"] {
+background: rgba(0,0,0,0);
+}
+
+[data-testid="stToolbar"] {
+right: 2rem;
+}
+
+</style>
+"""
+# add_logo("https://github.com/NebulaTris/vibescape/blob/main/logo.png?raw=true")
+st.markdown(page_bg_img, unsafe_allow_html=True)
+# st.title("Vibescape 🎉🎶")
+st.markdown('<h1 style="font-size: 32px; color: black;"> MoodTunes </h1>', unsafe_allow_html=True)
+st.sidebar.success("Select a page below.")
+# st.sidebar.text("Developed by Shambhavi")
+
+st.markdown("Step right up, fellow emotion voyager! Are you prepared to embark on a thrilling expedition through the vast spectrum of feelings? 🎢🎵")
+st.markdown("Welcome aboard Vibescape, where our cutting-edge AI technology intersects with the colorful tapestry of your emotions! We're equipped with our virtual goggles (metaphorically speaking, of course, but doesn't it add a dash of flair? 😎) to analyze your emotional landscape through your webcam. And what do we do with this treasure trove of emotions, you wonder? We transform them into bespoke playlists that are as invigorating as they are eclectic! 🕺💃")
+
+st.markdown("Have you heard of Spotify, SoundCloud, and YouTube? Brace yourself, because Vibescape seamlessly merges these titans of music into a single, unmissable entertainment extravaganza! Now, you can explore your favorite streaming platforms with a twist—tailored specifically to your mood! 🎶")
+
+st.markdown("Feeling as jubilant as a carefree panda today? Fear not, we've curated a playlist just for that! Or perhaps you find yourself enveloped in the melancholic embrace of the blues? Fret not, for Vibescape has got your back. Our AI prowess detects your vibes and serves up melodies that resonate with your moment. 🐼🎉                                                                                                                                                                                                                                                     ")
+
+# st.markdown("**So, get ready for a whirlwind of emotions and music. Vibescape is here to turn your webcam into a mood ring, your screen into a dance floor, and your heart into a DJ booth. What's next? Well, that's entirely up to you and your ever-changing feelings!**")
+# st.markdown("**So, strap in** 🚀 **, hit that webcam** 📷 **, and let the musical journey begin! Vibescape is your ticket to a rollercoaster of emotions, all set to your favorite tunes.** 🎢🎵")
+
+RTC_CONFIGURATION = RTCConfiguration(
+    {"iceServers": [{
+        "urls": ["stun:stun.l.google.com:19302"]
+    }]})
+
+# CWD path
+HERE = Path(__file__).parent
+
+model = load_model("model.h5")
+label = np.load("label.npy")
+
+holistic = mp.solutions.holistic
+hands = mp.solutions.hands
+holis = holistic.Holistic()
+drawing = mp.solutions.drawing_utils
+
+if "run" not in st.session_state:
+    st.session_state["run"] = ""
+
+run = np.load("emotion.npy")[0]
+
+try:
+    emotion = np.load("emotion.npy")[0]
+except:
+    emotion = ""
+
+    
+class EmotionProcessor(VideoProcessorBase):
+    def recv(self, frame: av.VideoFrame) -> av.VideoFrame:
+        frm = frame.to_ndarray(format="bgr24")
+        frm = cv2.flip(frm, 1)  
+        res = holis.process(cv2.cvtColor(frm, cv2.COLOR_BGR2RGB))
+        
+        lst = []
+        if res.face_landmarks:
+            for i in res.face_landmarks.landmark:
+                lst.append(i.x - res.face_landmarks.landmark[1].x)
+                lst.append(i.y - res.face_landmarks.landmark[1].y)
+        
+            if res.left_hand_landmarks:
+                for i in res.left_hand_landmarks.landmark:
+                    lst.append(i.x - res.left_hand_landmarks.landmark[8].x)
+                    lst.append(i.y - res.left_hand_landmarks.landmark[8].y)
+            else:
+                for i in range(42):
+                    lst.append(0.0)
+        
+            if res.right_hand_landmarks:
+                for i in res.right_hand_landmarks.landmark:
+                    lst.append(i.x - res.right_hand_landmarks.landmark[8].x)
+                    lst.append(i.y - res.right_hand_landmarks.landmark[8].y)
+            else:
+                for i in range(42):
+                    lst.append(0.0)
+        
+            lst = np.array(lst).reshape(1, -1)
+        
+            pred = label[np.argmax(model.predict(lst))]
+            print(pred)
+            cv2.putText(frm, pred, (50,50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,255,0), 2)
+            
+            np.save("emotion.npy",np.array([pred]))
+            
+            emotion = pred
+       
+        drawing.draw_landmarks(frm, res.face_landmarks, holistic.FACEMESH_CONTOURS)
+        drawing.draw_landmarks(frm, res.left_hand_landmarks, hands.HAND_CONNECTIONS) 
+        drawing.draw_landmarks(frm, res.right_hand_landmarks, hands.HAND_CONNECTIONS)
+    
+        return av.VideoFrame.from_ndarray(frm, format="bgr24")
+    
+
+
+webrtc_streamer(key="key", desired_playing_state=st.session_state.get("run", "") == "true" ,mode=WebRtcMode.SENDRECV,  rtc_configuration=RTC_CONFIGURATION, video_processor_factory=EmotionProcessor, media_stream_constraints={
+        "video": True,
+        "audio": False
+    },
+    async_processing=True)
+
+
+col1, col2, col6 = st.columns([1, 1, 1])
+
+with col1:
+    start_btn = st.button("Start")
+with col6:
+    stop_btn = st.button("Stop")
+
+if start_btn:
+    st.session_state["run"] = "true"
+    # st.experimental_rerun()
+    st.rerun()
+
+if stop_btn:
+    st.session_state["run"] = "false"
+    # st.experimental_rerun()
+    st.rerun()
+else:
+    if not emotion:
+        pass
+    else:
+        np.save("emotion.npy", np.array([""]))
+        st.session_state["emotion"] = run
+        st.success("Your current emotion is: " + emotion)
+        st.subheader("Choose your streaming service")
+
+col3, col4, col5 = st.columns(3)
+
+with col4:
+    btn = st.button("Spotify")
+    if btn:
+        switch_page("Spotify")
+
+with col5:
+    btn2 = st.button("Youtube")
+    if btn2:
+        switch_page("Youtube")
+
+with col3:
+    btn3 = st.button("Soundcloud")
+    if btn3:
+        switch_page("Soundcloud")
